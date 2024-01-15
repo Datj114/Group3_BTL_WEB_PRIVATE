8224523c1b3ccfd2c0516fda271f14d9d806c0d3
Bùi Văn Đạt <142251793+Datj114@users.noreply.github.com>
2024-01-15 09:49:32 +0700

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -8,3 +8,25 @@ close.addEventListener('click', function () {
     document.querySelector('.menu ul').style.transform = ' translateX(-270px)';
     document.querySelector('.menu ul').style.opacity = '0';
 });
+// When the user scrolls the page, execute myFunction
+window.addEventListener('scroll', function () {
+    myFunction();
+});
+
+// Get the header
+var header = document.querySelector('nav');
+
+// Get the offset position of the navbar
+// var sticky = header.offsetTop;
+// console.log(sticky);
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    var a = this.pageYOffset;
+    console.log(a);
+
+    if (a > 110) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
